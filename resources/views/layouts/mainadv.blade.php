d115a24cf02eed63971a02a4413052c2952bcc5b
Tarisa 1822250033 <tarisa@mhs.mdp.ac.id>
2021-06-02 16:34:10 +0700

    fix route

--- resources/views/layouts/mainadv.blade.php
+++ resources/views/layouts/mainadv.blade.php
@@ -53,7 +53,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/dashboard" class="brand-link">
       <img src="{{ asset('assets/img/icnwhite.png') }}" alt="SiMUDAH Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">SiMUDAH</span>
