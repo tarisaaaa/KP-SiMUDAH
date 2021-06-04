a98dd1c6d5ad8a575170fb5f468043b06d60deae
Tarisa 1822250033 <tarisa@mhs.mdp.ac.id>
2021-06-04 23:23:37 +0700

    finishing buku pedoman

--- app/Http/Controllers/BukuPanduanController.php
+++ app/Http/Controllers/BukuPanduanController.php
@@ -14,7 +14,23 @@ class BukuPanduanController extends Controller
         return response()->download('buku_panduan/BukuPanduanAdminAplikasi.pdf');
     }
 
+    public function adminkeuangan() {
+        return response()->download('buku_panduan/BukuPanduanAdminKeuangan.pdf');
+    }
+
+    public function ketuamahasiswa() {
+        return response()->download('buku_panduan/BukuPanduanKetuaMahasiswa.pdf');
+    }
+
     public function pembina() {
         return response()->download('buku_panduan/BukuPanduanPembina.pdf');
+    }
+
+    public function pelatih() {
+        return response()->download('buku_panduan/BukuPanduanPelatih.pdf');
+    }
+
+    public function wakilrektoriii() {
+        return response()->download('buku_panduan/BukuPanduanWRIII.pdf');
     }
 }
