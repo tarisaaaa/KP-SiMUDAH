70b027e1c0d2eff417d964d7e28cabda2a01097c
Tarisa 1822250033 <tarisa@mhs.mdp.ac.id>
2021-06-04 23:56:50 +0700

    fix delete absensi

--- database/migrations/2021_03_04_134538_create_absensi_detail_table.php
+++ database/migrations/2021_03_04_134538_create_absensi_detail_table.php
@@ -15,7 +15,11 @@ class CreateAbsensiDetailTable extends Migration
     {
         Schema::create('absensi_detail', function (Blueprint $table) {
             $table->id();
-            $table->string('absensi_id');
+            // $table->string('absensi_id');
+            $table->unsignedBigInteger('absensi_id');
+            $table->foreign('absensi_id')
+                ->references('id')->on('absensi')
+                ->onDelete('cascade');
             $table->string('anggota_id');
             $table->enum("status_absen", ['H', 'I', 'A']);
             $table->text('keterangan');
